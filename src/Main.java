89fb4c2c88c4fc249b7a11699aee84b60941df5a
winder <winder.gonzalez@uniminuto.edu.co>
2025-04-12 13:03:24 -0500

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("      &  &  & CALCULADORA &  &  & ");
+        System.out.println("¿Cuántas operaciones deseas realizar? ");
+        int numOperaciones = teclado.nextInt();
+        int max = 100;
+        int min = 1;
+
+        for(int i = 0; i < numOperaciones; ++i) {
+            int num1 = random.nextInt(max - min + 1) + min;
+            int num2 = random.nextInt(max - min + 1) + min;
+            System.out.println("");
+            System.out.println("*  *  *  *  *  *  *  *  * OPERACION "+ (i+1) +" *  *  *  *  *  *  *  *  *");
+            System.out.println("");
+            System.out.println("Introduce el signo aritmético (+, -, *, /, ^, %): ");
+            String signo = teclado.next();
+            double resultado = 0;
+            System.out.println("#  #  #  #  #  # RESULTADOS #  #  #  #  #  # ");
+            switch (signo) {
+                case "+":
+
+                    resultado = (double)(num1 + num2);
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                case "-":
+                    resultado = (double)(num1 - num2);
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                case "*":
+                    resultado = (double)(num1 * num2);
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                case "/":
+                    resultado = (double)num1 / (double)num2;
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                case "^":
+                    resultado = Math.pow((double)num1, (double)num2);
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                case "%":
+                    resultado = (double)(num1 % num2);
+                    System.out.println("el numero 1 es : " + num1);
+                    System.out.println("el numero 2 es : " + num2);
+                    break;
+                default:
+                    System.out.println("Signo aritmético no válido.");
+                    continue;
+            }
+
+            System.out.println("Resultado: " + resultado);
+
+        }
+
     }
 }
