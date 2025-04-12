cd66a61ae00164bc7a40842f53a27ee8fa97d1ff
winder <winder.gonzalez@uniminuto.edu.co>
2025-04-12 16:43:00 -0500

    resolucion problemas

--- src/Candidatos.java
+++ src/Candidatos.java
@@ -1,21 +1,33 @@
 public class Candidatos {
-   private int votosTotal;
+
    private candidato1 candidato1;
    private Candidato2 candidato2;
    private Candidato3 candidato3;
    public Candidatos (){
-       votosTotal=0;
-
+         candidato1 =new candidato1();
+         candidato2 =new Candidato2();
+         candidato3 =new Candidato3();
    }
-
-   public int porcentajeCanditato1(){
-       return (candidato1.getVoto()/votosTotal)*100;
-   }
-    public int porcentajeCanditato2(){
-        return (candidato2.getVoto()/votosTotal)*100;
+    public int votosTotal(){
+       int votosTotal=candidato1.getVoto()+candidato2.getVoto()+candidato3.getVoto();
+       return votosTotal;
     }
-    public int porcentajeCanditato3(){
-        return (candidato3.getVoto()/votosTotal)*100;
+   public double porcentajeCanditato1(){
+       double r1=votosTotal();
+       double r2= candidato1.getVoto() ;
+       return (r2/r1)*100;
+
+   }
+    public double porcentajeCanditato2(){
+
+        double r1=votosTotal();
+        double r2= candidato2.getVoto() ;
+        return (r2/r1)*100;
+    }
+    public double porcentajeCanditato3(){
+        double r1=votosTotal();
+        double r2= candidato3.getVoto() ;
+        return (r2/r1)*100;
     }
 
     public int costoPromedioCampanas(){
@@ -29,9 +41,7 @@ public class Candidatos {
        candidato3.vaciarUrnas();
     }
 
-    public int getVotosTotal() {
-        return votosTotal;
-    }
+
 
     public candidato1 getCandidato1() {
         return candidato1;
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     static boolean estado=true;
-    private static Candidatos candidatos;
+    static Candidatos candidatos ;
     public static void main(String[] args) {
+        candidatos =new Candidatos();
         do{
             menu();
         }while(estado);
@@ -16,27 +17,35 @@ public class Main {
     public static void menu(){
         Scanner in =new Scanner(System.in);
 
-        System.out.println("&  &  &  &  MENU  &  &  &  &");
-        System.out.println("1)Votar");
-        System.out.println("2)Mostrar Resultados");
-        System.out.println("3)Vaciar urnas");
-        System.out.println("4)Salir ");
+        System.out.println("\n  &  &  &  &|  MENU  |&  &  &  &");
+        System.out.println("    1)Votar");
+        System.out.println("    2)Mostrar Resultados");
+        System.out.println("    3)Vaciar urnas");
+        System.out.println("    4)Salir ");
+        System.out.print(" Ingrese una opcion : ");
         int opc = in.nextInt();
         switch (opc){
             case 1 ->{
-                System.out.println("&  &  &  &  VOTA POR UN CANDIDATO   &  &  &  &");
+                boolean estadov=true;
+                do{
+                System.out.println("\n *  *  *|  VOTA POR UN CANDIDATO  |*  *  * ");
                 System.out.println("elija un candidato");
-                System.out.println("1) "+candidatos.getCandidato1().getNombre());
-                System.out.println("2) "+candidatos.getCandidato2().getNombre());
-                System.out.println("3) "+candidatos.getCandidato3().getNombre());
-
+                System.out.println("    1) "+candidatos.getCandidato1().getNombre());
+                System.out.println("    2) "+candidatos.getCandidato2().getNombre());
+                System.out.println("    3) "+candidatos.getCandidato3().getNombre());
+                System.out.println("    4)Regresar <- ");
+                    System.out.print(" Ingrese una opcion : ");
                 int c =in.nextInt();
+                    if(c!=1 & c!=2 &c!=3){
+                        estadov=false;
+                    }else{
+                System.out.println("---------|-------------------|---------------------|--------");
+                System.out.println("\nelija el medio de eleccion");
 
-                System.out.println("elija el medio de eleccion");
-
-                System.out.println("1)Internet");
-                System.out.println("2)Radio");
-                System.out.println("3)television");
+                System.out.println("    1)Internet");
+                System.out.println("    2)Radio");
+                System.out.println("    3)television");
+                        System.out.print(" Ingrese una opcion : ");
                 int m =in.nextInt();
 
                 if (c==1){
@@ -45,21 +54,25 @@ public class Main {
                     candidatos.getCandidato2().votarCandidato(m);
                 }if (c==3){
                     candidatos.getCandidato3().votarCandidato(m);
-                }
+                }}
+                }while(estadov);
             }
             case 2->{
-                System.out.println("&  &  &  &  Motrar Resultados    &  &  &  &");
+                System.out.println("\n  WWWWWWW|  MOSTRAR RESULTADOS   |WWWWWW");
+                if(candidatos.votosTotal()!=0 ) {
+                    System.out.println("Votos Totales : " + candidatos.votosTotal() + "\n");
+                    System.out.println("Porcentaje Votos Candidato 1 : " + candidatos.porcentajeCanditato1()+"%");
+                    System.out.println("Porcentaje Votos Candidato 2 : " + candidatos.porcentajeCanditato2()+"%");
+                    System.out.println("Porcentaje Votos Candidato 3 : " + candidatos.porcentajeCanditato3()+"%" + "\n");
 
-                System.out.println("Votos Totales : "+candidatos.getVotosTotal()+"\n");
-                System.out.println("Porcentaje Votos Candidato 1 : "+candidatos.porcentajeCanditato1());
-                System.out.println("Porcentaje Votos Candidato 2 : "+candidatos.porcentajeCanditato2());
-                System.out.println("Porcentaje Votos Candidato 3 : "+candidatos.porcentajeCanditato3()+"\n");
-
-                System.out.println("Costo promedio Campaña : "+candidatos.costoPromedioCampanas());
+                    System.out.println("Costo promedio Campaña : " + candidatos.costoPromedioCampanas());
+                }else{
+                    System.out.print("\nXXXXXXXX| TODAVIA NO HA VOTADO NADIE |XXXXXXXX"+"\n");
+                }
             }
             case 3->{
                 candidatos.vaciarUrnas();
-                System.out.println("## se han vaciado las urnas ## ");
+                System.out.println("\n## SE HAN VACIADO LAS URNAS  ## ");
             }
             case 4->{
                 estado=false;
